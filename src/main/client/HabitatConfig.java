4fe2922733ef83535fcefeafdd3b93ddc0f9c9b4
jjf28 <forsbergjj@gmail.com>
2014-11-11 17:04:33 -0600

    Initial Upload

---
+++ src/main/client/HabitatConfig.java
@@ -0,0 +1,14 @@
+package main.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class HabitatConfig implements EntryPoint {
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		Window.alert("Hello World!");
+	}
+}
