406eabde59b478c2cbb24336e52db535cbab7e43
Jeremiah Strong <stron125@d.umn.edu>
2014-11-19 12:21:59 -0600

    Added this to one line

--- src/main/client/ConfigOptions.java
+++ src/main/client/ConfigOptions.java
@@ -18,7 +18,7 @@ public class ConfigOptions {
 	 */
 	public ConfigOptions() {
 		
-		loginRequired = true;
+		this.loginRequired = true;
 		loginNameCaseSensative = false;
 		loginPassCaseSensative = true;
 		loginName = "mars";
